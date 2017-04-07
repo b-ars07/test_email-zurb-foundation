d896e19858db3d151eb45d0d149113a2281ec6af
Arseniy Berezin <b-ars07@yandex.ru>
2017-04-08 01:48:17 +0300

    Сверстал письмо адаптивно

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,33 +1,33 @@
-var
-         gulp = require('gulp'),
-         sass = require('gulp-sass'),
-         inky = require('inky'),
-    inlineCss = require('gulp-inline-css'); 
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    inky = require('inky'),
+    inlineCss = require('gulp-inline-css');
 
-//styles
+
+//STYLES
 gulp.task('styles', function () {
-    return gulp.src('../scss/style.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('../dist'));
+  return gulp.src('../scss/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('../dist'));
 });
 
-//convert inky
-gulp.task('inky', function () {
-    return gulp.src('../templates/**/*.html')
-        .pipe(inky())
-        .pipe(gulp.dest('../dist'));
+//CONVERTE INKY
+gulp.task('inky', function() {
+  return gulp.src('../templates/**/*.html')
+    .pipe(inky())
+    .pipe(gulp.dest('../dist'));
 });
 
-//inline css
+//INLINE CSS
 gulp.task('inline', function () {
-    return gulp.src('../dist/*.html')
+  return gulp.src('../dist/*.html')
         .pipe(inlineCss())
         .pipe(gulp.dest('../dist/inlined'));
-    
 });
 
-//watch
-gulp.task('default', function () {
+
+//WATCH
+gulp.task('default',function() {
     gulp.watch('../scss/**/*.scss',['styles']);
     gulp.watch('../templates/**/*.html',['inky']);
 });
